5d0cd6567840c529775e18ff03e0e470522a2087
scottbot95 <scotttechau@gmail.com>
2021-09-10 23:24:55 -0700

    Create basic uninstall script

---
+++ programs/uninstall_faultytools.lua
@@ -0,0 +1,11 @@
+local tFilesToDelete = {
+    "modules",
+    "programs"
+}
+
+local sInstallDir = settings.get("faultyTools.installDir", "/")
+
+fs.delete("/startup")
+for _, sPath in ipairs(tFilesToDelete) do
+    fs.delete(sInstallDir .. '/' .. sPath)
+end
